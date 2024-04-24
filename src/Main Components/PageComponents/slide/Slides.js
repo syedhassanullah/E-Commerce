67d6c71c95811cfa9eb702872cc1eee373101049
Your Name <syedhassanullah.0900@gmail.com>
2024-04-24 23:47:34 +0500

    some changes

--- src/Main Components/PageComponents/slide/Slides.js
+++ src/Main Components/PageComponents/slide/Slides.js
@@ -6,8 +6,16 @@ import './Slides.css';
 const Slides = () => {
   return (
     <section className='slidemain'>
-        <div className='slide1'></div>
-        <div className='slide2'></div>
+        <div class="parallelogram"></div>
+        <div className='sliderighttext'>
+          <p>Women Collection</p>
+          <h1>Peaky Blinders</h1>
+          <h4>Discription</h4>
+          <p>Before pushing, it's recommended to ensure your local 
+            branch is up to date with the remote branch to avoid conflicts. 
+            You can do this by pulling the latest changes from the remote using git pull.</p>
+          <button className='blackbt1'>Buy Now</button>
+        </div>
     </section>
   )
 }
